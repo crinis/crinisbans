e7794bdd31cd3e88e3f5afc96099572b3f22b834
crinis <crinis@users.noreply.github.com>
2017-04-24 09:54:47 +0200

    #4 Fix bug in cronjob activation

--- wordpress/crinisbans/classes/Controller/Cron.php
+++ wordpress/crinisbans/classes/Controller/Cron.php
@@ -4,7 +4,7 @@ namespace crinis\cb\Controller;
 class Cron {
 
 	public function enable_cronjobs() {
-		if ( wp_next_scheduled( 'cb_cache_server_data' === false ) ) {
+		if ( wp_next_scheduled( 'cb_cache_server_data' ) === false ) {
 			wp_schedule_event( time(), '30sec', 'cb_cache_server_data' );
 		}
 		return wp_next_scheduled( 'cb_cache_server_data' ) ? true:false;
