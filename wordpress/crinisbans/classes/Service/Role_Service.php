b8155518bc59cac0a040f52a8297a47310d5bd3b
Crinis <postmaster@crinis.org>
2017-07-05 01:20:00 +0200

    Add user role management to Role_Service

--- wordpress/crinisbans/classes/Service/Role_Service.php
+++ wordpress/crinisbans/classes/Service/Role_Service.php
@@ -2,6 +2,7 @@
 namespace crinis\cb\Service;
 
 class Role_Service {
+
     public function add_role(Group $group) {
         $role = $this->get_role_name($group);
         return add_role( $role, $role );
@@ -20,4 +21,20 @@ class Role_Service {
         $role = $this->get_role($group);
         return $GLOBALS['wp_roles']->is_role( $role );
     }
+
+    public function add_role_to_user($admin,$role){
+        $user = new WP_User( $admin->get_user_id() );
+        return $user->add_role( $role );
+    }
+
+    public function remove_role_from_user($admin,$role){
+        $user = new WP_User( $admin->get_user_id() );
+        return $user->remove_role( $role );
+    }
+
+    public function has_user_role($admin,$role){
+        $user = new WP_User( $admin->get_user_id() );
+        return in_array( $role, (array) $user->roles );
+    }
+
 }
